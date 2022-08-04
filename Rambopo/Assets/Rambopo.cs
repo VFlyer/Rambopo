3f81ab48e62ec4019f22528a6204b876e8afc3f2
Apolo Simon <62307787+Fartlicker777@users.noreply.github.com>
2022-08-04 09:50:29 -0500

    Release build

--- Rambopo/Assets/Rambopo.cs
+++ Rambopo/Assets/Rambopo.cs
@@ -7,21 +7,20 @@ using UnityEngine;
 using KModkit;
 using Rnd = UnityEngine.Random;
 
-public class Rambopo : MonoBehaviour
-{
+public class Rambopo : MonoBehaviour {
 
-    public KMBombInfo Bomb;
-    public KMAudio Audio;
+   public KMBombInfo Bomb;
+   public KMAudio Audio;
 
-    public SpriteRenderer LeftSpriteR;
-    public SpriteRenderer RightSpriteR;
+   public SpriteRenderer LeftSpriteR;
+   public SpriteRenderer RightSpriteR;
 
-    public KMSelectable SwitchKMS;
-    public GameObject SwitchObj;
+   public KMSelectable SwitchKMS;
+   public GameObject SwitchObj;
 
-    public Sprite[] Sprites;
+   public Sprite[] Sprites;
 
-    int[][][] Tables = {
+   int[][][] Tables = {
       new int[][] {
       new int[] {8, 0, 3, 6, 2},
       new int[] {9, 7, 3, 5, 8},
@@ -72,245 +71,234 @@ public class Rambopo : MonoBehaviour
       new int[] {67, 60, 65, 68, 64}
       }
    };
-    int[] RatioList = { 11, 12, 13, 21, 23, 31, 32 };
+   int[] RatioList = { 11, 12, 13, 21, 23, 31, 32 };
 
-    int LeftCycle;
-    int RightCycle;
-    int Row;
-    int Col;
+   int LeftCycle;
+   int RightCycle;
+   int Row;
+   int Col;
 
-    int[] SelectedPair = { 0, 0 };
+   int[] SelectedPair = { 0, 0 };
 
-    int LeftFakePairs;
-    int RightFakePairs;
+   int LeftFakePairs;
+   int RightFakePairs;
 
-    float CommonTime;
+   float CommonTime;
 
-    List<int> LeftScreen = new List<int>() { };
-    List<int> RightScreen = new List<int>() { };
+   List<int> LeftScreen = new List<int>() { };
+   List<int> RightScreen = new List<int>() { };
 
-    int LAnswer;
-    int RAnswer;
+   int LAnswer;
+   int RAnswer;
 
-    Coroutine CyclingL;
-    Coroutine CyclingR;
+   Coroutine CyclingL;
+   Coroutine CyclingR;
 
-    //int IndexofCycle;
+   //int IndexofCycle;
 
-    static int ModuleIdCounter = 1;
-    int ModuleId;
-    private bool ModuleSolved;
+   static int ModuleIdCounter = 1;
+   int ModuleId;
+   private bool ModuleSolved;
 
-    private bool _switchDir;
-    private int _count;
+   private bool _switchDir;
+   private int _count;
 
-    void Awake()
-    {
-        ModuleId = ModuleIdCounter++;
-        /*
-        foreach (KMSelectable object in keypad) {
-            object.OnInteract += delegate () { keypadPress(object); return false; };
-        }
-        */
+   void Awake () {
+      ModuleId = ModuleIdCounter++;
+      /*
+      foreach (KMSelectable object in keypad) {
+          object.OnInteract += delegate () { keypadPress(object); return false; };
+      }
+      */
 
-        SwitchKMS.OnInteract += delegate () { SwitchFlip(); return false; };
+      SwitchKMS.OnInteract += delegate () { SwitchFlip(); return false; };
 
-    }
+   }
 
-    void SwitchFlip()
-    {
-        _switchDir = !_switchDir;
-        Audio.PlaySoundAtTransform("Switch" + (_switchDir ? "1" : "2"), transform);
-        StartCoroutine(SwitchFlipAnimation(_switchDir ? 0 : 30, _switchDir ? 30 : 0));
-        if (LeftSpriteR.sprite == Sprites[LAnswer] && RightSpriteR.sprite == Sprites[RAnswer])
-        {
-            Solve();
-        }
-        else
-        {
-            Strike();
-        }
-    }
+   void SwitchFlip () {
+      _switchDir = !_switchDir;
+      Audio.PlaySoundAtTransform("Switch" + (_switchDir ? "1" : "2"), transform);
+      StartCoroutine(SwitchFlipAnimation(_switchDir ? 0 : 30, _switchDir ? 30 : 0));
+      if (LeftSpriteR.sprite == Sprites[LAnswer] && RightSpriteR.sprite == Sprites[RAnswer]) {
+         Solve();
+      }
+      else {
+         Strike();
+      }
+   }
 
-    void Solve()
-    {
-        GetComponent<KMBombModule>().HandlePass();
-        if (CyclingL != null)
-            StopCoroutine(CyclingL);
-        if (CyclingR != null)
-            StopCoroutine(CyclingR);
-        ModuleSolved = true;
-    }
+   void Solve () {
+      GetComponent<KMBombModule>().HandlePass();
+      if (CyclingL != null)
+         StopCoroutine(CyclingL);
+      if (CyclingR != null)
+         StopCoroutine(CyclingR);
+      ModuleSolved = true;
+   }
 
-    void Strike()
-    {
-        GetComponent<KMBombModule>().HandleStrike();
-    }
+   void Strike () {
+      GetComponent<KMBombModule>().HandleStrike();
+   }
 
-    void Start()
-    {
-        LeftFakePairs = Rnd.Range(4, 7);
-        RightFakePairs = Rnd.Range(4, 7);
-        Generate();
-    }
+   void Start () {
+      LeftFakePairs = Rnd.Range(3, 5);
+      RightFakePairs = Rnd.Range(3, 5);
+      //LeftFakePairs = 3;
+      //RightFakePairs = 3;
+      Generate();
+   }
 
-    void Generate()
-    {
-        LeftCycle = Rnd.Range(1, 4);
-        RightCycle = Rnd.Range(1, 4);
-        CommonTime = Rnd.Range(2f, 3.5f);
-        Row = Rnd.Range(0, 5);
-        Col = Rnd.Range(0, 4);
+   void Generate () {
+      LeftCycle = Rnd.Range(1, 4);
+      RightCycle = Rnd.Range(1, 4);
+      //LeftCycle = 3;
+      //RightCycle = 2;
+      //CommonTime = .3f;
+      CommonTime = Rnd.Range(1.5f, 2.5f);
+      Row = Rnd.Range(0, 5);
+      Col = Rnd.Range(0, 4);
 
-        if (LeftCycle == RightCycle)
-        {
+      if (LeftCycle == RightCycle) {
+         if (Rnd.Range(0, 7) == 0) {
             LeftCycle = 1;
             RightCycle = 1;
-
             CommonTime = Rnd.Range(.75f, 1f);
-        }
-        Debug.LogFormat("[Rambopo #{0}] The ratio is {1}/{2}", ModuleId, LeftCycle, RightCycle);
-        /*try {
-           LeftScreen.Add(Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col]);
-           RightScreen.Add(Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col + 1]);
-        }
-        catch (Exception) {
-           Debug.Log(LeftCycle * 10 + RightCycle);
-           Debug.Log(Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle));
-           throw;
-        }*/
+         }
+         else {
+            do {
+               LeftCycle = Rnd.Range(1, 4);
+               RightCycle = Rnd.Range(1, 4);
+            } while (LeftCycle == RightCycle);
+         }
+      }
 
-        LAnswer = Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col];
-        RAnswer = Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col + 1];
+      /*if (LeftCycle + RightCycle == 5) {
+         LeftFakePairs = 4;
+         RightFakePairs = 4;
+      }*/
+      Debug.LogFormat("[Rambopo #{0}] The ratio is {1}/{2}", ModuleId, LeftCycle, RightCycle);
+      /*try {
+         LeftScreen.Add(Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col]);
+         RightScreen.Add(Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col + 1]);
+      }
+      catch (Exception) {
+         Debug.Log(LeftCycle * 10 + RightCycle);
+         Debug.Log(Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle));
+         throw;
+      }*/
 
-        Debug.LogFormat("[Rambopo #{0}] The correct pair should be {1}, {2}", ModuleId, Sprites[LAnswer].name, Sprites[RAnswer].name);
+      LAnswer = Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col];
+      RAnswer = Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col + 1];
 
-        LeftScreen.Add(Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col]);
-        RightScreen.Add(Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col + 1]);
+      Debug.LogFormat("[Rambopo #{0}] The correct pair should be {1}, {2}.", ModuleId, Sprites[LAnswer].name, Sprites[RAnswer].name);
+      Debug.LogFormat("[Rambopo #{0}] There are {1} fakes on the left and {2} on the right.", ModuleId, LeftFakePairs, RightFakePairs);
+      LeftScreen.Add(Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col]);
+      RightScreen.Add(Tables[Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle)][Row][Col + 1]);
 
-        Fakes();
-    }
+      Fakes();
+   }
 
-    void Fakes()
-    {
-        int[] temp = Enumerable.Range(0, 70).ToArray().Shuffle();
-        int index = 0;
+   void Fakes () {
+      int[] temp = Enumerable.Range(0, 70).ToArray().Shuffle();
+      int index = 0;
 
 
-        while (LeftScreen.Count() != 1 + LeftFakePairs)
-        {
-            for (int i = 0; i < LeftScreen.Count(); i++)
-            {
-                if (temp[index] / 10 == LeftScreen[0] / 10 && DuplicateChecker(Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle), temp[index], LeftScreen[i]))
-                {
-                    continue;
-                }
+      while (LeftScreen.Count() != 1 + LeftFakePairs) {
+         for (int i = 0; i < LeftScreen.Count(); i++) {
+            if ((temp[index] / 10 == LeftScreen[0] / 10 && DuplicateChecker(Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle), temp[index], LeftScreen[i])) || LeftScreen.Contains(temp[index])) {
+               continue;
             }
-            LeftScreen.Add(temp[index]);
-            index++;
-        }
+         }
+         LeftScreen.Add(temp[index]);
+         index++;
+      }
 
-        while (RightScreen.Count() != 1 + RightFakePairs)
-        {
-            for (int i = 0; i < RightScreen.Count(); i++)
-            {
-                if (temp[index] / 10 == RightScreen[0] / 10 && DuplicateChecker(Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle), temp[index], RightScreen[i]))
-                {
-                    continue;
-                }
+      while (RightScreen.Count() != 1 + RightFakePairs) {
+         for (int i = 0; i < RightScreen.Count(); i++) {
+            if ((temp[index] / 10 == RightScreen[0] / 10 && DuplicateChecker(Array.IndexOf(RatioList, LeftCycle * 10 + RightCycle), RightScreen[i], temp[index])) || RightScreen.Contains(temp[index])) {
+               continue;
             }
-            RightScreen.Add(temp[index]);
-            index++;
-        }
+         }
+         RightScreen.Add(temp[index]);
+         index++;
+      }
 
-        _count = Rnd.Range(0, 5);
-        CyclingL = StartCoroutine(CycleLeft());
-        CyclingR = StartCoroutine(CycleRight());
-    }
+      //_count = Rnd.Range(0, 5); DO NOT CHANGE IT FROM 0. IT CAN MAKE THE MOD IMPOSSIBLE IF DONE SO.
+      _count = 0;
+      CyclingL = StartCoroutine(CycleLeft());
+      CyclingR = StartCoroutine(CycleRight());
+   }
 
-    bool DuplicateChecker(int T, int x, int y)
-    {
+   bool DuplicateChecker (int T, int x, int y) {
 
-        foreach (int[] R in Tables[T])
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (x == R[i] && y == R[i + 1])
-                {
-                    return true;
-                }
+      foreach (int[] R in Tables[T]) {
+         for (int i = 0; i < 4; i++) {
+            if (x == R[i] && y == R[i + 1]) {
+               return true;
             }
-        }
+         }
+      }
 
-        return false;
-    }
+      return false;
+   }
 
-    IEnumerator CycleLeft()
-    {
-        int Count = _count;
-        while (true)
-        {
-            LeftSpriteR.sprite = Sprites[LeftScreen[Count % LeftScreen.Count()]];
-            Count++;
-            yield return new WaitForSeconds(CommonTime / (float)LeftCycle);
-        }
-    }
+   IEnumerator CycleLeft () {
+      int Count = _count;
+      while (true) {
+         LeftSpriteR.sprite = Sprites[LeftScreen[Count % LeftScreen.Count()]];
+         Count++;
+         yield return new WaitForSeconds(CommonTime / (float) LeftCycle);
+      }
+   }
 
-    IEnumerator CycleRight()
-    {
-        int Count = _count;
-        while (true)
-        {
-            RightSpriteR.sprite = Sprites[RightScreen[Count % RightScreen.Count()]];
-            Count++;
-            yield return new WaitForSeconds(CommonTime / (float)RightCycle);
-        }
-    }
+   IEnumerator CycleRight () {
+      int Count = _count;
+      while (true) {
+         RightSpriteR.sprite = Sprites[RightScreen[Count % RightScreen.Count()]];
+         Count++;
+         yield return new WaitForSeconds(CommonTime / (float) RightCycle);
+      }
+   }
 
-    private bool _togglingSwitch;
-    private IEnumerator SwitchFlipAnimation(float from, float to)
-    {
-        while (_togglingSwitch)
-            yield return null;
-        _togglingSwitch = true;
+   private bool _togglingSwitch;
+   private IEnumerator SwitchFlipAnimation (float from, float to) {
+      while (_togglingSwitch)
+         yield return null;
+      _togglingSwitch = true;
 
-        var cur = from;
-        var stop = false;
-        while (!stop)
-        {
-            cur += 250 * Time.deltaTime * (to > from ? 1 : -1);
-            if ((to > from && cur >= to) || (to < from && cur <= to))
-            {
-                cur = to;
-                stop = true;
-            }
-            SwitchObj.transform.localEulerAngles = new Vector3(90, 0, cur);
-            yield return null;
-        }
-        _togglingSwitch = false;
-    }
+      var cur = from;
+      var stop = false;
+      while (!stop) {
+         cur += 250 * Time.deltaTime * (to > from ? 1 : -1);
+         if ((to > from && cur >= to) || (to < from && cur <= to)) {
+            cur = to;
+            stop = true;
+         }
+         SwitchObj.transform.localEulerAngles = new Vector3(90, 0, cur);
+         yield return null;
+      }
+      _togglingSwitch = false;
+   }
 
 #pragma warning disable 414
-    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+   private readonly string TwitchHelpMessage = @"Use !{0} guess to randomly flip. It won't strike if you are lucky.";
 #pragma warning restore 414
 
-    IEnumerator ProcessTwitchCommand(string Command)
-    {
-        yield return null;
-    }
+   IEnumerator ProcessTwitchCommand (string Command) {
+      yield return null;
+      if (Command == "guess") {
+         SwitchKMS.OnInteract();
+      }
+   }
 
-    IEnumerator TwitchHandleForcedSolve()
-    {
-        while (!ModuleSolved)
-        {
-            if (LeftSpriteR.sprite == Sprites[LAnswer] && RightSpriteR.sprite == Sprites[RAnswer])
-            {
-                SwitchKMS.OnInteract();
-            }
-            else
-            {
-                yield return true;
-            }
-        }
-    }
+   IEnumerator TwitchHandleForcedSolve () {
+      while (!ModuleSolved) {
+         if (LeftSpriteR.sprite == Sprites[LAnswer] && RightSpriteR.sprite == Sprites[RAnswer]) {
+            SwitchKMS.OnInteract();
+         }
+         else {
+            yield return true;
+         }
+      }
+   }
 }
